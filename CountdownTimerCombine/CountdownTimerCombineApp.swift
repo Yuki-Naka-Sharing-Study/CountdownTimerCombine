25ab43b14c2c077a565ea601383cae85930e894e
仲優樹 <yu-ki216m@outlook.jp>
2025-06-25 15:04:42 +0900

    Initial Commit

---
+++ CountdownTimerCombine/CountdownTimerCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownTimerCombineApp.swift
+//  CountdownTimerCombine
+//
+//  Created by 仲優樹 on 2025/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CountdownTimerCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
